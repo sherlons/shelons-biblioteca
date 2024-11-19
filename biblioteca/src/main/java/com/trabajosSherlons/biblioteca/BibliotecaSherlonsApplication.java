ae7be2d4abb8b53eb0a9a816e3b1aafbbf0495fe
sherlons <lukasteoo03@gmail.com>
2024-11-19 15:09:00 -0500

    creacion biblioteca

---
+++ biblioteca/src/main/java/com/trabajosSherlons/biblioteca/BibliotecaSherlonsApplication.java
@@ -0,0 +1,13 @@
+package com.trabajosSherlons.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaSherlonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaSherlonsApplication.class, args);
+	}
+
+}
